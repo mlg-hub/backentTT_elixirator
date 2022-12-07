96a803a3190f95f2b4bb14361ea065fd3094f41c
Ongala morisho <mlgwebhost@gmail.com>
2022-12-07 13:49:05 +0200

    Init commit

---
+++ lib/fuel_calc.ex
@@ -0,0 +1,18 @@
+defmodule FuelCalc do
+  @moduledoc """
+  Documentation for `FuelCalc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FuelCalc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
